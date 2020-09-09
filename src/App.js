8301472754ad4668033c301501777c73d1ffc241
Marjan Stojanovski <macikb8@yahoo.com>
2020-09-09 19:03:30 +0200

    add posts with API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import './App.css';
+import { client } from './client';
+import Posts from './components/Posts';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    articles:[]
+  }
+
+  componentDidMount() {
+    client.getEntries()
+    .then((response) => {
+      console.log(response)
+      this.setState({
+        articles: response.items
+      })
+    })
+    .catch(console.error)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <header>
+            <div className="wrapper">
+              <span>React and Contentful</span>
+            </div>
+          </header>
+          <main>
+            <div className="wrapper">
+              <Posts posts= {this.state.articles}/>
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
