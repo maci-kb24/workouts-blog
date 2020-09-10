65eddab3d77bf0a23abd87acad4819513b478110
Marjan Stojanovski <macikb8@yahoo.com>
2020-09-10 23:58:29 +0200

    Updates

---
+++ precache-manifest.214cb760010b374df4e475081959b526.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f529290704b041bb80c6061496c04bb",
+    "url": "/workouts-blog/index.html"
+  },
+  {
+    "revision": "3372b0f873ad778b0eda",
+    "url": "/workouts-blog/static/css/main.472899a0.chunk.css"
+  },
+  {
+    "revision": "e07c9f870cb83b06ae49",
+    "url": "/workouts-blog/static/js/2.d3eb570e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/workouts-blog/static/js/2.d3eb570e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3372b0f873ad778b0eda",
+    "url": "/workouts-blog/static/js/main.136c5e60.chunk.js"
+  },
+  {
+    "revision": "44d79875ff432c059329",
+    "url": "/workouts-blog/static/js/runtime-main.c625e93a.js"
+  },
+  {
+    "revision": "37e5cf56b71cd8b2f4caca373c2cb007",
+    "url": "/workouts-blog/static/media/gym-bg.37e5cf56.jpeg"
+  }
+]);
